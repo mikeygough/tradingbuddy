377f24216956c53a1b47ef3e8ecc4e620e5023db
mikey <mikey@hey.com>
2023-01-09 10:52:23 -0600

    add pdf statistic writing

--- 2-pandas.py
+++ 2-pandas.py
@@ -2,6 +2,7 @@ from config import *
 import pandas as pd
 import numpy as np
 import databento as db
+from fpdf import FPDF
 
 df = pd.read_csv('static/data.csv')
 
@@ -29,7 +30,7 @@ def expected_range(s, v, dte, y=365):
     dte: days to expiration
     y: trading period (one year) '''
     return s * v * np.sqrt(dte / y)
-    
+
 UPPER_1 = CLOSE + expected_range(CLOSE, HISTORICAL_VOL, 1)
 UPPER_2 = CLOSE + expected_range(CLOSE, HISTORICAL_VOL, 2)
 UPPER_3 = CLOSE + expected_range(CLOSE, HISTORICAL_VOL, 3)
@@ -42,22 +43,133 @@ LOWER_4 = CLOSE - expected_range(CLOSE, HISTORICAL_VOL, 4)
 LOWER_5 = CLOSE - expected_range(CLOSE, HISTORICAL_VOL, 5)
 
 # print
-print('close', CLOSE)
-print('high', HIGH)
-print('low', LOW)
-print('return', RETURN)
-print('one_sd', ONE_SD)
-print('two_sd', TWO_SD)
-print('three_sd', THREE_SD)
-print('historical_vol', HISTORICAL_VOL)
-print('one day upper range', UPPER_1)
-print('two day upper range', UPPER_2)
-print('three day upper range', UPPER_3)
-print('four day upper range', UPPER_4)
-print('five day upper range', UPPER_5)
-print('one day lower range', LOWER_1)
-print('two day lower range', LOWER_2)
-print('three day lower range', LOWER_3)
-print('four day lower range', LOWER_4)
-print('five day lower range', LOWER_5)
+# print('close', CLOSE)
+# print('high', HIGH)
+# print('low', LOW)
+# print('return', RETURN)
+# print('one_sd', ONE_SD)
+# print('two_sd', TWO_SD)
+# print('three_sd', THREE_SD)
+# print('historical_vol', HISTORICAL_VOL)
+# print('one day upper range', UPPER_1)
+# print('two day upper range', UPPER_2)
+# print('three day upper range', UPPER_3)
+# print('four day upper range', UPPER_4)
+# print('five day upper range', UPPER_5)
+# print('one day lower range', LOWER_1)
+# print('two day lower range', LOWER_2)
+# print('three day lower range', LOWER_3)
+# print('four day lower range', LOWER_4)
+# print('five day lower range', LOWER_5)
+
+# initialize pdf
+pdf = FPDF(orientation='L')
+
+# the origin is at the upper-left corner and the current position is by default placed at 1 cm from the borders; the margins can be changed with set_margins.
+pdf.add_page()
+
+# set font
+pdf.set_font('Arial', 'B', 16)
+
+# a cell is a rectangular area, possibly framed, which contains some text. It is output at the current position. We specify its dimensions, its text (centered or aligned), if borders should be drawn, and where the current position moves after it (to the right, below or to the beginning of the next line)
+# TITLE
+pdf.cell(w=40, h=10, txt='/ES',
+         border=0, ln=0, align='',
+         fill=False, link='')
+# add line break
+# h = the height of the break.
+pdf.ln(h='')
+
+# STATISTICS
+# print('close', CLOSE)# close
+pdf.cell(w=40, h=10, txt='Close: {}'.format(CLOSE),
+         border=0, ln=1, align='',
+         fill=False, link='')
+
+# print('high', HIGH)
+pdf.cell(w=40, h=10, txt='Period High: {}'.format(HIGH),
+    ln=1)
+
+# print('low', LOW)
+pdf.cell(w=40, h=10, txt='Period Low: {}'.format(LOW),
+    ln=1)
+
+# print('return', RETURN)
+pdf.cell(w=40, h=10, txt='Return: {}'.format(RETURN),
+    ln=1)
+
+# print('one_sd', ONE_SD)
+pdf.cell(w=40, h=10, txt='One Sd.: {}'.format(ONE_SD),
+    ln=1)
+
+# print('two_sd', TWO_SD)
+pdf.cell(w=40, h=10, txt='Two Sd.: {}'.format(TWO_SD),
+    ln=1)
+
+# print('three_sd', THREE_SD)
+pdf.cell(w=40, h=10, txt='Three Sd.: {}'.format(THREE_SD),
+    ln=1)
+
+# print('historical_vol', HISTORICAL_VOL)
+pdf.cell(w=40, h=10, txt='Historical Volatility: {}'.format(HISTORICAL_VOL),
+    ln=1)
+
+# print('one day upper range', UPPER_1)
+pdf.cell(w=40, h=10, txt='Upper 1: {}'.format(UPPER_1),
+    ln=1)
+
+# print('two day upper range', UPPER_2)
+pdf.cell(w=40, h=10, txt='Upper 2: {}'.format(UPPER_2),
+    ln=1)
+
+# print('three day upper range', UPPER_3)
+pdf.cell(w=40, h=10, txt='Upper 3: {}'.format(UPPER_3),
+    ln=1)
+
+# print('four day upper range', UPPER_4)
+pdf.cell(w=40, h=10, txt='Upper 4: {}'.format(UPPER_4),
+    ln=1)
+
+# print('five day upper range', UPPER_5)
+pdf.cell(w=40, h=10, txt='Upper 5: {}'.format(UPPER_5),
+    ln=1)
+
+# print('one day lower range', LOWER_1)
+pdf.cell(w=40, h=10, txt='Lower 1: {}'.format(LOWER_1),
+    ln=1)
+
+# print('two day lower range', LOWER_2)
+pdf.cell(w=40, h=10, txt='Lower 2: {}'.format(LOWER_2),
+    ln=1)
+
+# print('three day lower range', LOWER_3)
+pdf.cell(w=40, h=10, txt='Lower 3: {}'.format(LOWER_3),
+    ln=1)
+
+# print('four day lower range', LOWER_4)
+pdf.cell(w=40, h=10, txt='Lower 4: {}'.format(LOWER_4),
+    ln=1)
+
+# print('five day lower range', LOWER_5)
+pdf.cell(w=40, h=10, txt='Lower 5: {}'.format(LOWER_5),
+    ln=1)
+
+
+pdf.cell(60, 10, txt='Powered by FPDF.', border=0, ln=1, align='C')
+pdf.output('tuto1.pdf', 'F')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
